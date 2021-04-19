9c7973743ed4acda46010e9173ee0c4fe36f0fc0
l4zs <68546083+l4zs@users.noreply.github.com>
2021-04-19 22:52:22 +0200

    patch issue
    - patch issue with java version 8 and below
    - add a warning message for java version 8 and below

--- search-java-minecraft-start.bat
+++ search-java-minecraft-start.bat
@@ -16,7 +16,7 @@ set /a xmx=2048
 set /a xms=2048
 
 :: change this to specified java min / max version
-set /a min=15
+set /a min=6
 set /a max=15
 
 
@@ -30,26 +30,36 @@ set /a max=15
 set currentPath=%cd%
 set javaPath=%CD:~0,3%
 echo.
-echo ---------------------------------
-echo              Values:
+echo  ---------------------------------
+echo               Values:
 echo.
-echo   jar-name: %jar%
-echo   xms: %xms%
-echo   xmx: %xmx%
+echo    jar-name: %jar%
+echo    xms: %xms%
+echo    xmx: %xmx%
 if %min% EQU %max% (
-echo   java version: %min%
+echo    java version: %min%
 ) else (
-echo   java version between %min% and %max%
+echo    java version between %min% and %max%
 )
-echo ---------------------------------
+echo  ---------------------------------
 echo.
-echo   created by:
-echo   l4zs
+echo    created by:
+echo    l4zs
 echo.
-echo ---------------------------------
-echo   Searching for java versions..
-echo   this may take several minutes
-echo ---------------------------------
+echo  ---------------------------------
+echo    Searching for java versions..
+echo    this may take several minutes
+echo  ---------------------------------
+echo.
+if %min% LEQ 8 (
+echo.
+echo  ------- WARNING -------
+echo.
+echo  your selected Java version is pretty outdated, if you don't need to use this version I strongly recommend using a more recent version.
+echo.
+echo  ------- WARNING -------
+echo.
+)
 cd %javaPath%
 :: loop through all java.exe's
 for /f %%i in ('dir /b /s java.exe') do call :javaCheck %%i
@@ -86,20 +96,28 @@ set build=%3
 if %min% LEQ 8 (
 if %major% EQU 1 (
 if %minor% GEQ %min% (
-echo FOUND JAVA VERSION: %minor%
+if %minor% LEQ %max% (
+if %minor% LEQ 8 (
+echo  FOUND JAVA VERSION: %minor%
 echo.
-echo Creating start.bat
+echo  Creating start.bat
 echo.
 call :createBat
 goto End
 )
+goto End
 )
+goto End
+)
+goto End
+)
+goto End
 )
 if %major% GEQ %min% (
 if %major% LEQ %max% (
-echo FOUND JAVA VERSION: %major%
+echo  FOUND JAVA VERSION: %major%
 echo.
-echo Creating start.bat
+echo  Creating start.bat
 echo.
 call :createBat
 goto End
@@ -112,22 +130,27 @@ goto End
 :createBat
 set content=%javaPath% -Xms%xms%M -Xmx%xmx%M -jar %jar% nogui
 echo %content%>%currentPath%\start.bat
-echo start.bat created!
+echo  start.bat created!
+echo.
 if %eula% == true (
 echo eula=true>%currentPath%\eula.txt
-echo eula accepted!
+echo  eula accepted!
+echo.
 )
 goto Exit
 
 
 :noJavaVersion
-echo No Java is installed or there is no Version between Java %min% and Java %max%
-echo Please download it from https://adoptopenjdk.net/installation.html
+echo  No version between Java %min% and Java %max% found.
+echo.
+echo  Please download it from https://adoptopenjdk.net/installation.html
+echo.
 goto Exit
 
 
 :Exit
-echo Programm will exit now
+echo  Programm will exit now
+echo.
 pause
 exit
 goto eof
